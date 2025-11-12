080f0e78e722b01c4f742ff1e8e589027ff28e41
GuilhermeB <guilhermebazani7@gmail.com>
2025-11-12 13:00:03 -0300

    Protótipo inicial

---
+++ Cadastro_Coin.py
@@ -0,0 +1,33 @@
+from Funções_Coin import acoes
+
+usuarios = {}
+saldos = {}
+
+
+def cadastro():
+    print("CADASTRO")
+    nome = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+
+    if nome in usuarios:
+        print("Usuário já cadastrado. Escolha outro nome.")
+        return
+    usuarios[nome] = senha
+    saldos[nome] = {"corrente": 0.0, "poupanca": 0.0}
+    print(f"Usuário '{nome}' cadastrado com sucesso!")
+
+
+def login():
+    print("LOGIN")
+    nome = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+
+    if nome in usuarios and usuarios[nome] == senha:
+        print(f"Login bem-sucedido! Bem-vindo, {nome}!")
+        acoes(nome)
+
+    elif nome in usuarios:
+        print("Senha incorreta. Tente novamente.")
+    else:
+        print("Usuário não encontrado. Faça o cadastro primeiro.")
+
